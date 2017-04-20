4306948d0e45e0d8c3b50e414a581b7256198246
Rich Gomez <0xkione@gmail.com>
2017-04-20 06:21:12 -0600

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,7 @@
+// index.android.js
+
+import { AppRegistry } from 'react-native';
+
+import App from './src/app/';
+
+AppRegistry.registerComponent('tech_stack', () => App);
---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+// src/app.index.js
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hi!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
